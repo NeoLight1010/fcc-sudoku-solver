cf4b8d74abc0488691bb616a16aa23010c85794f
Anthony Suárez <neolight1010@gmail.com>
2021-03-22 15:44:49 -0500

    Add unit tests and error messages

--- controllers/sudoku-solver.js
+++ controllers/sudoku-solver.js
@@ -1,3 +1,11 @@
+export const INVALID_CHAR_ERROR = "Invalid characters in puzzle";
+export const INVALID_LENGTH_ERROR =  "Expected puzzle to be 31 characters long";
+export const CANNOT_SOLVE_ERROR = "Puzzle cannot be solved";
+
+export const ROW_CONFLICT_ERROR = "Row conflict found";
+export const COLUMN_CONFLICT_ERROR = "Column conflict found";
+export const REGION_CONFLICT_ERROR = "Region conflict found";
+
 class SudokuSolver {
 
   validate(puzzleString) {
--- tests/1_unit-tests.js
+++ tests/1_unit-tests.js
@@ -1,9 +1,160 @@
-const chai = require('chai');
+const chai = require("chai");
 const assert = chai.assert;
 
-const Solver = require('../controllers/sudoku-solver.js');
-let solver;
+const Solver = require("../controllers/sudoku-solver.js");
+const {
+  puzzleString,
+  puzzlesAndSolutions,
+} = require("../controllers/puzzle-strings");
+const { expect } = require("chai");
+const solver = new Solver();
 
-suite('UnitTests', () => {
+// Logic handles a valid puzzle string of 81 characters
+// Logic handles a puzzle string with invalid characters (not 1-9 or .)
+// Logic handles a puzzle string that is not 81 characters in length
+// Logic handles a valid row placement
+// Logic handles an invalid row placement
+// Logic handles a valid Col placement
+// Logic handles an invalid Col placement
+// Logic handles a valid region (3x3 grid) placement
+// Logic handles an invalid region (3x3 grid) placement
+// Valid puzzle strings pass the solver
+// Invalid puzzle strings fail the solver
+// Solver returns the the expected solution for an incomplete puzzzle
 
+suite("UnitTests", () => {
+  suite("SudokuSolver.validate(puzzleString) tests", () => {
+    test("Valid puzzle string of 81 characters", (done) => {
+      const output = solver.validate(puzzlesAndSolutions[0][0]);
+
+      assert.isTrue(output.valid);
+      assert.notExists(output.error);
+      done();
+    });
+
+    test("Invalid puzzle string with invalid characters", (done) => {
+      const output = solver.validate(
+        puzzlesAndSolutions[0][0].replace(".", "?")
+      );
+
+      assert.isFalse(output.valid);
+      assert.equal(output.error, Solver.INVALID_CHAR_ERROR);
+      done();
+    });
+
+    test("Invalid puzzle with length not equal to 81", (done) => {
+      const output = solver.validate(puzzlesAndSolutions[0][0].substring(40));
+
+      assert.isFalse(output.valid);
+      assert.equal(output.error, Solver.INVALID_LENGTH_ERROR);
+      done();
+    });
+  });
+
+  suite('"Check" functions tests', () => {
+    test("Valid row placement", (done) => {
+      const output = solver.checkRowPlacement(
+        puzzlesAndSolutions[0][0],
+        4,
+        1,
+        3
+      );
+
+      assert.isTrue(output.valid);
+      assert.notExists(output.error);
+      done();
+    });
+
+    test("Invalid row placement", (done) => {
+      const output = solver.checkRowPlacement(
+        puzzlesAndSolutions[0][0],
+        1,
+        2,
+        1
+      );
+
+      assert.isFalse(output.valid);
+      assert.equal(output.error, Solver.ROW_CONFLICT_ERROR);
+      done();
+    });
+
+    test("Valid Col placement", (done) => {
+      const output = solver.checkColPlacement(
+        puzzlesAndSolutions[0][0],
+        3,
+        1,
+        5
+      );
+
+      assert.isTrue(output.valid);
+      assert.notExists(output.error);
+      done();
+    });
+
+    test("Invalid Col placement", (done) => {
+      const output = solver.checkColPlacement(
+        puzzlesAndSolutions[0][0],
+        5,
+        4,
+        1
+      );
+
+      assert.isFalse(output.valid);
+      assert.equal(output.error, Solver.Col_CONFLICT_ERROR);
+      done();
+    });
+
+    test("Valid region placement", (done) => {
+      const output = solver.checkRegionPlacement(
+        puzzlesAndSolutions[0][0],
+        2,
+        2,
+        7
+      );
+
+      assert.isTrue(output.valid);
+      assert.notExists(output.error);
+      done();
+    });
+
+    test("Invalid region placement", (done) => {
+      const output = solver.checkRegionPlacement(
+        puzzlesAndSolutions[0][0],
+        4,
+        4,
+        8
+      );
+
+      assert.isFalse(output.valid);
+      assert.equal(output.error, Solver.REGION_CONFLICT_ERROR);
+      done();
+    });
+  });
+
+  suite('Solver.solve(puzzleString) tests', () => {
+    test('Valid puzzle string', (done) => {
+      const output = solver.validate(puzzlesAndSolutions[0][0]);
+
+      assert.exists(output.solution);
+      assert.notExists(output.error);
+      done();
+    });
+
+    test('Invalid puzzle string', (done) => {
+      const output = solver.solve(puzzlesAndSolutions[0][0].slice(5));
+
+      assert.exists(output.solution);
+      assert.equal(output.error, Solver.INVALID_LENGTH_ERROR);
+      done();
+    });
+
+    test('Return correct solution', (done) => {
+      const output = solver.solve(puzzlesAndSolutions[0][0]);
+      const expected = puzzlesAndSolutions[0][1];
+
+      assert.equal(output.solution, expected);
+      assert.notExists(output.error);
+      done();
+    });
+  });
 });
