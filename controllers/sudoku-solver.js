c75f60f88fac80aa53da5d50b92363c7bcdb8f6f
Anthony Suárez <neolight1010@gmail.com>
2021-03-25 11:49:25 -0500

    Fix functions

--- controllers/sudoku-solver.js
+++ controllers/sudoku-solver.js
@@ -20,15 +20,15 @@ export class SudokuSolver {
 
   checkRowPlacement(puzzleString, row, column, value) {
     // Find row start and end
-    let rowStart = 1, rowEnd = 1;
+    let rowStart = 0, rowEnd = 9;
     for (let i=1; i<=9; i++) {
-      rowStart *= i;
-      rowEnd *= i + 1;
+      if ((row-1)*9 + (column-1) <= rowEnd-1 && (row-1)*9 + (column-1) >= rowStart-1) break
 
-      if (row <= rowEnd && row >= rowStart) break
+      rowStart += 9;
+      rowEnd += 9;
     }
 
-    let rowString = puzzleString.slice(rowStart - 1, column - 1) + puzzleString.slice(column, rowEnd);
+    let rowString = puzzleString.slice(rowStart, rowStart + column - 1) + puzzleString.slice(rowStart + column, rowEnd);
 
     if ((rowString.includes)(value)) return {valid: false, error: ROW_CONFLICT_ERROR};
     return {valid: true};
@@ -38,7 +38,7 @@ export class SudokuSolver {
     let columnString = "";
 
     for (let i=1; i<=9; i++) {
-      if (i!=row) columnString += puzzleString[(column - 1) + 9 * i];
+      if (i!=row) columnString += puzzleString[(column - 1) + 9 * (i-1)];
     }
 
     if (columnString.includes(value)) return {valid: false, error: COLUMN_CONFLICT_ERROR};
@@ -46,24 +46,18 @@ export class SudokuSolver {
   }
 
   checkRegionPlacement(puzzleString, row, column, value) {
-     // Find region start and end
-    let regionStart = 1, regionEnd = 1;
-    for (let i=1; i<=3; i++) {
-      regionStart *= i;
-      regionEnd *= i + 1;
+    const regionStartRow = (row - 1) - ((row - 1) % 3) + 1;   
+    const regionStartCol = (column - 1) - ((column - 1) % 3) + 1;
 
-      if (row <= regionEnd && row >= regionStart) break;
+    let regionString = '';
+    for (let i=0; i<3; i++) {
+      regionString += puzzleString.substring((regionStartCol - 1) + 9*(regionStartRow - 1) + 9*i, ((regionStartCol - 1) + 9*(regionStartRow - 1) + 9*i) + 3);
     }
 
-    // Get regionString
-    let regionString;
-    for (let i=1; i<=3; i++) {
-      regionString += puzzleString.substring((regionStart - 1) * i + (column - 1),
-                                              regionEnd * i + (column - 1));
-    }
-
-    // Remove selected tile
-    regionString = regionString.slice(0, 3 * (row - 1) + column) + regionString.slice(column + 1);
+    // Remove selected value
+    const inRegionCoords = [row - regionStartRow, column - regionStartCol];
+    const inRegionStringIndex = inRegionCoords[0]*3 + inRegionCoords[1]
+    regionString = regionString.slice(0, inRegionStringIndex) + regionString.slice(inRegionStringIndex + 1);
 
     if (regionString.includes(value)) return {valid: false, error: REGION_CONFLICT_ERROR};
     return {valid: true};
@@ -73,5 +67,38 @@ export class SudokuSolver {
     // Perform checks
     const validation = this.validate(puzzleString);
     if (!validation.valid) return {error: validation.error};
+
+    // Convert puzzleString to array
+    let puzzleArray = []
+    for (let i=1; i<=9; i++) {
+      puzzleArray.push(puzzleString.substring((i-1) * 9, i*9));
+    }
+
+    // Find dots coordinates in puzzleArray
+    let dotCoords = [];
+    for (let i=1; i<=9; i++) {
+      for (let j=1; j<=9; j++) {
+        if (puzzleArray[i-1][j-1] == '.') dotCoords.push([i, j]);
+      }
+    }
+
+    // Try with all numbers
+    let coord = dotCoords[0];
+    for (let i=1; i<=9; i++) {
+      let rowCheck = this.checkRowPlacement(puzzleString, coord[0], coord[1], i);
+      let colCheck = this.checkColPlacement(puzzleString, coord[0], coord[1], i);
+      let regionCheck = this.checkRegionPlacement(puzzleString, coord[0], coord[1], i);
+
+      if (rowCheck.valid && colCheck.valid && regionCheck.valid) {
+        if (dotCoords.length == 1) return {solution: puzzleString.replace('.', i.toString())};
+
+        let newSolve = this.solve(puzzleString.replace('.', i.toString()));
+
+        if (newSolve.solution && !newSolve.error) {
+          return {solution: newSolve.solution.replace('.', i.toString())};
+        }
+      }
+    }
+    return {error: CANNOT_SOLVE_ERROR};
   }
 }
